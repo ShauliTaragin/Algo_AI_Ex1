510f0ea3c507d8df5790209ab4b163a7a8083ab5
ShauliTaragin <shaulitaragin@gmail.com>
2021-11-05 12:29:55 +0200

    trying to read from a file

--- src/main.java
+++ src/main.java
@@ -1,5 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class main {
     public static void main(String[] args) {
-        System.out.println("hello world");
+
+
+    }
+    public static void readfromfile(String name){
+        try {
+            FileReader fr = new FileReader(name);
+            BufferedReader br = new BufferedReader(fr);
+            System.out.println(br.readLine());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        catch(IOException e ){
+            e.printStackTrace();
+        }
+    }
+    public void addclass(int a){
+        /*
+        **
+        *
+        *
+
+         */
     }
 }
